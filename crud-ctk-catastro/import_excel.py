295590fb3ce1d1dd4fb20a337df9f205e91207ae
Nelson <villalobosnelson000@gmail.com>
2024-11-15 18:17:24 -0400

    Import excel function update

--- crud-ctk-catastro/import_excel.py
+++ crud-ctk-catastro/import_excel.py
@@ -8,8 +8,13 @@ engine = create_engine(connection_url)
 
 excel_file = r'C:\Github\crud-ctk-catastro\crud-ctk-catastro\file.xlsx'
 df_excel = pd.read_excel(excel_file, sheet_name=None)
+import pandas as pd
 
-df_excel_cleaned = {sheet_name: sheet_data.dropna(how='all') 
-                    for sheet_name, sheet_data in df_excel.items()}
+# Load the Excel file (sheet_name=None loads all sheets into a dictionary of DataFrames)
+df_excel = pd.read_excel(excel_file, sheet_name=None)
 
-print(df_excel)
+# Replace NaN with empty string or other placeholder value in all sheets
+df_excel_cleaned = {sheet_name: sheet_data.fillna('') for sheet_name, sheet_data in df_excel.items()}
+
+# Now df_excel_cleaned contains the cleaned data where NaN values are replaced
+print(df_excel_cleaned)
